ec3332618749a4ec87f7c65f1071d097798ec6cc
atico-print <aticomercedes@gmail.com>
2026-05-08 18:22:02 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+var CACHE='atico-v1';
+var FILES=['/','/index.html'];
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES);}));
+});
+self.addEventListener('fetch',function(e){
+  e.respondWith(fetch(e.request).catch(function(){return caches.match(e.request);}));
+});
