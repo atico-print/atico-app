9bbe27e676000c09f1e438001cc9331ae9464973
atico-print <aticomercedes@gmail.com>
2026-05-08 20:08:40 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,8 @@
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-});
+self.addEventListener('install', function(e) { self.skipWaiting(); });
 self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
-    })
-  );
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+  }));
 });
 self.addEventListener('fetch', function(e) {
   e.respondWith(fetch(e.request).catch(function() { return caches.match(e.request); }));
